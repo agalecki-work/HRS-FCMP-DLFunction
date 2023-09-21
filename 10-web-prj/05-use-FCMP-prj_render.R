4c662017001f685ad720763f120f8fcb3c38d725
agalecki <agalecki@umich.edu>
2023-09-21 11:58:10 -0400

    2 changed

--- 10-web-prj/05-use-FCMP-prj_render.R
+++ 10-web-prj/05-use-FCMP-prj_render.R
@@ -1,7 +1,7 @@
 # source("05-use-FCMP-prj_render.R")
 
 rm(list= ls())
-bnm   <- "05-use-FCMP-prj_render"
+bnm   <- "05-use-FCMP-prj" # .Rmd
 nmRmd <- paste0(bnm, ".Rmd")
 nmR   <- paste0(bnm,".Rprog")
 rmarkdown::render(nmRmd, "all")
